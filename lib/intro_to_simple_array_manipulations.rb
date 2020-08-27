ec7758051e40e7b99bfc989a98c03f483874a149
Elias Delgado <evdelgado99@gmail.com>
2020-08-27 08:15:08 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+# using push
+
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+next_color = "violet"
+colors_in_the_rainbow.push(next_color)
+
+# using unshift
+
+# using pop
+
+# pop with args
+
+# using shift
+
+# shift with args
+
