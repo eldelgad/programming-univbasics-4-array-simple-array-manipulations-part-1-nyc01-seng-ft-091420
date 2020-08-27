2e83f3a37bc711ad9299780f203f404887c35d6c
Elias Delgado <evdelgado99@gmail.com>
2020-08-27 08:25:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,34 +1,34 @@
-# using push
+# using_push
 
 def using_push(array, string)
   array.push(string)
 end
 
-# using unshift
+# using_unshift
 
 def using_unshift(array, string)
   array.unshift(string)
 end
 
-# using pop
+# using_pop
 
 def using_pop(array)
   array.pop()
 end
 
-# pop with args
+# pop_with_args
 
 def pop_with_args(array)
   array.pop(2)
 end
 
-# using shift
+# using_shift
 
 def using_shift(array)
   array.shift()
 end
 
-# shift with args
+# shift_with_args
 
 def shift_with_args(array)
   array.shift(2)
